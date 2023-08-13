3808c7e65e3c5e0ff25a2faa10af9c6c1322ca69
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2023-08-13 22:13:19 +0100

    launch

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
 		<div id="projects" className="w-full">
 			<div className="max-w-[1240] mx-auto px-2 py-16">
 				<p className="text-xl tracking-widest uppercase">Projects</p>
-				<h2 className="py-4 text-purple-600">What I've Built</h2>
+				<h2 className="py-4 text-purple-600">What I&apos;ve Built</h2>
 				<div className="grid md:grid-cols-2 gap-8">
 					<ProjectItem
 						title="Binge Reviews"
