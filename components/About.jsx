2e764d8d7fcf25c4d7580c0ee2b8d608a1f37beb
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2023-08-13 16:47:04 +0100

    add about component

--- components/About.jsx
+++ components/About.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import Image from "next/image";
-import AboutImg from "../public/assets/images/AboutImg.png";
+import AboutImg from "../public/assets/images/AboutImg.jpg";
 
 const About = () => {
 	return (
 		<div className="w-full md:h-screen p-2 flex items-center py-16">
 			<div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
 				<div className="col-span-2">
-					<p>About</p>
-					<h2>Who I Am</h2>
-					<p>
+					<p className="uppercase text-xl tracking-widest ">About</p>
+					<h2 className="text-purple-600 py-4">Who I Am</h2>
+					<p className="py-2">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ad amet
 						corporis quia odit nihil dolorem dolores deserunt quae laborum at,
 						facilis, iure ipsam a consequuntur ut facere veritatis! Odio.
 					</p>
+					<p className="py-2 underline cursor-pointer">
+						Check out my latest{" "}
+						<span className="text-purple-600">Projects</span>
+					</p>
 				</div>
-				<div>
-					<Image src={AboutImg} alt="/" width="500" height="500" />
+				<div className="w-full h-auto m-auto border border-purple-600 rounded-xl items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+					<Image
+						src={AboutImg}
+						alt="/"
+						width="500"
+						height="500"
+						className="rounded-xl"
+					/>
 				</div>
 			</div>
 		</div>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Navbar from '../components/Navbar'
 import Main from '../components/Main'
 import About from '../components/About'
 
-
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Gethin Davies Full Stack Developer</title>
         <meta name="description" content="Home page" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <Main />
